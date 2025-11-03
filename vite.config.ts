e2893f3a4fa79347097ca83c4bfa3f3d4be2f674
Harith <harithpradeepan@gmail.com>
2025-11-03 09:45:55 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
-  import tailwindcss from 'tailwindcss';
+  import tailwindcss from '@tailwindcss/vite'
   import path from 'path';
 
   export default defineConfig({
